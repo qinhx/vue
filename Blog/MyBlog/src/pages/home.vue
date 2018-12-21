8a3d709ef39c6748d64ba64cfeb772ce0af5e6b8
qhx <835643409@qq.com>
2018-12-21 11:06:22 +0800

    blog

--- Blog/MyBlog/src/pages/home.vue
+++ Blog/MyBlog/src/pages/home.vue
@@ -3,7 +3,11 @@
         <el-card  v-for="(i,key) in detail" shadow="hover" :body-style="{width:'250px',float:'left',padding:'0',height:'300px'}" :key="key">
             
             <h5 style="text-align:center">{{i.title[0]}}</h5>
-                <router-link :to="{name:'detial',params:{id:i._id}}" tag='a'>详细</router-link>
+                <p style="text-align:center">[{{i.readtimes[0]}}]</p>
+                <br>
+                <br>
+                <br>
+               <small>{{i.times[0]}}</small> <router-link :to="{name:'detial',params:{id:i._id}}" tag='a'>详细</router-link>
         </el-card>
     </div>
 </template>
@@ -35,6 +39,11 @@
     .homeApp{
         margin: 0 auto;
         width: 80%;
+        overflow: hidden;
+    }
+    h5{
+        white-space: nowrap;
+        text-overflow: ellipsis;
         overflow: hidden;
     }
 </style>
