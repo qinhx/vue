c9cf1d31ff1eb877e056f87d7fba16fb27415eb1
qinhx <835643409@qq.com>
2019-03-26 00:21:21 +0800

    be

---
+++ wxbf/routes/record.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var router=  express.Router();
+var mongoose = require('mongoose');
+var getopenid = require('../public/javascripts/requetP')
+var db = mongoose.createConnection('mongodb://www.qinhx.cn:27017/wx',{useNewUrlParser:true});
+var record = db.model('record',{
+    openid:String,
+    days:String,
+    payOrNot:String,
+    hours:String,
+    cost:String,
+    LP:Array,
+    money:String
+})
+router.route('').get((req,res,next)=>{
+    record.findById(req.query.id,(err,data)=>{
+        if(err) res.send(err);
+        res.send(data)
+    })
+}).post((req,res,next)=>{
+    getopenid(req.body.code).then(data=>{
+        options = { upsert: true,new:true,useFindAndModify:false};
+        record.findOneAndUpdate({
+            "openid":data
+        },{
+            "openid":data
+        },
+        options,
+        (err,raw)=>{
+            if(err) {
+                res.send(err);
+            }
+            else {
+                var obj = {
+                    days:raw.days,
+                    id: raw._id,
+                    payOrNot:raw.payOrNot,
+                    hours: raw.hours,
+                    cost:raw.cost,
+                    LP:raw.LP,
+                    money:raw.money
+                }
+                res.send(obj)
+            }
+        })
+    }).catch(err=>{
+        res.send(err)
+    })
+}).delete((req,res,next)=>{
+    record.remove({},(err,raw)=>{
+        res.send(raw)
+    })
+}).put((req,res,next)=>{
+    var options = {overwrite :false,new:true,useFindAndModify:false}
+    record.findOneAndUpdate(
+        {"_id":mongoose.Types.ObjectId(req.body.id)}
+        ,req.body,options,(err,raw)=>{
+        if(err) res.send(err);
+        else {
+            var obj = {
+                days:raw.days,
+                id: raw._id,
+                payOrNot:raw.payOrNot,
+                hours: raw.hours,
+                cost:raw.cost,
+                LP:raw.LP,
+                money:raw.money
+            }
+            res.send(obj)
+        }
+    })
+})
+module.exports = router;
